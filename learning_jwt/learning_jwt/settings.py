f7df002828722c5a1148598a2db1d97686760a11
NikFMatten <nfmatten@gmail.com>
2023-04-26 15:01:26 -0600

    feat: added rest framework project settings

--- learning_jwt/learning_jwt/settings.py
+++ learning_jwt/learning_jwt/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -112,6 +113,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
+}
+
 
 try:
     from learning_jwt.local_settings import *
